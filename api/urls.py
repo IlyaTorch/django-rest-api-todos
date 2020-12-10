57fd7ab46061bb284ba6d3d75f8952b39f259e3a
IlyaTorch <itorch2001@gmail.com>
2020-12-10 23:08:44 +0300

    add crud api for a singlo todo

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('todos', views.TodoCreateList.as_view()),
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
     path('todos/completed', views.TodoCompletedList.as_view()),
-
 ]
