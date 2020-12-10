3a9078707bf2a6b5ae84fc7e09408df06a3503b9
IlyaTorch <itorch2001@gmail.com>
2020-12-11 00:08:49 +0300

    add token authentication

--- api/views.py
+++ api/views.py
@@ -1,7 +1,30 @@
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoCompleteSerializer
 from todo.models import Todo
 from django.utils import timezone
+from rest_framework.parsers import JSONParser
+from django.http import JsonResponse
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+
+@csrf_exempt
+def sing_up(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(data['username'], password=data['password'])
+            user.save()
+
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse(
+                {'error': 'That username has already been taken. Please choose a new username'},
+                status=400
+            )
 
 
 class TodoCreateList(generics.ListCreateAPIView):
