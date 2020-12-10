f70beabdc0724b20a19f3bff03a7f2e56d241b80
IlyaTorch <itorch2001@gmail.com>
2020-12-11 00:31:57 +0300

    add login using tokens

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Todos
     path('todos', views.TodoCreateList.as_view()),
     path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
     path('todos/<int:pk>/complete', views.TodoComplete.as_view()),
     path('todos/completed', views.TodoCompletedList.as_view()),
 
+    # Auth
     path('signup', views.sing_up),
+    path('login', views.login_user),
 ]
--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from django.http import JsonResponse
 from django.db import IntegrityError
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
 
 @csrf_exempt
@@ -25,6 +26,25 @@ def sing_up(request):
                 {'error': 'That username has already been taken. Please choose a new username'},
                 status=400
             )
+
+
+@csrf_exempt
+def login_user(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(request, username=data['username'], password=data['password'])
+
+        if user is None:
+            return JsonResponse(
+                {'error': 'Could not login. Check username and password'},
+                status=400
+            )
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=200)
 
 
 class TodoCreateList(generics.ListCreateAPIView):
